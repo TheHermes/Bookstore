31b39dc39d270c2b65c7693936d28eab39a42e6e
TheHermes <hkvheik@gmail.com>
2024-07-17 16:13:39 +0300

    Added bookstore

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import AddTodo from './AddTodo';
+
+import './App.css'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import './App.css'
+
+function App() {
+  const [books, setBooks] = useState([])
+
+  const columnDefs = [
+    { field: 'title', sortable: true, filter: true },
+    { field: 'author', sortable: true, filter: true },
+    { field: 'year', sortable: true, filter: true },
+    { field: 'isbn', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true },
+    {
+      headerName: '',
+      field: 'id',
+      width: 90,
+      cellRenderer: params =>
+        <IconButton onClick={() => deleteBook(params.value)} size="small" color="error">
+          <DeleteIcon />
+        </IconButton>
+    }
+  ]
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems = () => {
+    fetch(`https://bookstore-2cdf9-default-rtdb.europe-west1.firebasedatabase.app/books/.json`)
+      .then(response => response.json())
+      .then(data => addKeys(data))
+      .catch(err => console.error(err))
+  }
+
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) =>
+      Object.defineProperty(item, 'id', { value: keys[index] }));
+    setBooks(valueKeys);
+  }
+
+  const addBook = (newBook) => {
+    fetch('https://bookstore-2cdf9-default-rtdb.europe-west1.firebasedatabase.app/books/.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(newBook)
+    })
+    .then(response => fetchItems())
+    .catch(err => console.log(err))
+  }
+
+  const deleteBook = (id) => {
+    fetch(`https://bookstore-2cdf9-default-rtdb.europe-west1.firebasedatabase.app/books/${id}.json`,
+      {
+        method: 'DELETE',
+      })
+      .then(response => fetchItems())
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <div className='App'>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant='h5'>
+            Bookstore
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <AddTodo addBook={addBook}/>
+      <div
+        className="ag-theme-material"
+        style={{ height: 700, width: 1200}}>
+        <AgGridReact
+          rowData={books}
+          columnDefs={columnDefs}
+        />
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
